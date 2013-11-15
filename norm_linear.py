e0047f78f56ca2f43c806b2b645bb3870cfc0138
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-11-14 23:36:41 -0500

    Initial commit

---
+++ norm_linear.py
@@ -0,0 +1,30 @@
+import random
+
+import numpy
+
+from data import homo, lumo, energy, dipole, gap
+
+def normalize(X):
+    mu = X.mean(0)
+    sigma = X.std(0)
+    return (X-mu)/sigma, mu, sigma
+
+for offset in xrange(10, 200, 10):
+    order = range(len(homo))
+    random.shuffle(order)
+    homo = homo[order]
+    lumo = lumo[order]
+    energy = energy[order]
+    dipole = dipole[order]
+    gap = gap[order]
+
+    X = numpy.concatenate((homo[:offset], lumo[:offset], energy[:offset], dipole[:offset]), 1)
+    X, mu, sigma = normalize(X)
+    X = numpy.concatenate((X, numpy.ones(homo[:offset].shape)), 1)
+    theta = (X.T*X).I*X.T*gap[:offset]
+
+    X2 = numpy.concatenate((homo[offset:], lumo[offset:], energy[offset:], dipole[offset:]), 1)
+    X2 = (X2-mu)/sigma
+    estimate = numpy.concatenate((X2, numpy.ones(homo[offset:].shape)),1)*theta
+    temp = (gap[offset:] - estimate)
+    print (temp*temp.T).mean()
