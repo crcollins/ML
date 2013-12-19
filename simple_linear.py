c9d6bc9f0dcd5fbd0b2c4e8ee048569b049bd678
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-12-19 03:33:09 -0500

    Remove for loop from linear regressions

--- simple_linear.py
+++ simple_linear.py
@@ -4,17 +4,18 @@ import numpy
 
 from data import homo, lumo, energy, dipole, gap
 
-for offset in xrange(10, 200, 10):
-    order = range(len(homo))
-    random.shuffle(order)
-    homo = homo[order]
-    lumo = lumo[order]
-    energy = energy[order]
-    dipole = dipole[order]
-    gap = gap[order]
+offset = 200
+order = range(len(homo))
+random.shuffle(order)
+homo = homo[order]
+lumo = lumo[order]
+energy = energy[order]
+dipole = dipole[order]
+gap = gap[order]
 
-    X = numpy.concatenate((homo[:offset], lumo[:offset], energy[:offset], dipole[:offset], numpy.ones(homo[:offset].shape)), 1)
-    theta = (X.T*X).I*X.T*gap[:offset]
-    estimate = numpy.concatenate((homo[offset:], lumo[offset:], energy[offset:], dipole[offset:], numpy.ones(homo[offset:].shape)),1)*theta
-    temp = (gap[offset:] - estimate)
-    print (temp*temp.T).mean()
+X = numpy.concatenate((homo[:offset], lumo[:offset], energy[:offset], dipole[:offset], numpy.ones(homo[:offset].shape)), 1)
+theta = (X.T*X).I*X.T*gap[:offset]
+print theta
+estimate = numpy.concatenate((homo[offset:], lumo[offset:], energy[offset:], dipole[offset:], numpy.ones(homo[offset:].shape)),1)*theta
+temp = (gap[offset:] - estimate)
+print (temp*temp.T).mean()
