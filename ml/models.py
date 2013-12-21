14500ebef4af23a3c46408c131c66aeb4a33b441
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-12-21 01:56:19 -0500

    Update models to have size limits

--- ml/models.py
+++ ml/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Name(models.Model):
-    pass
+    name = models.CharField(max_length=300)
 
 class StandardName(Name):
-    left_name = models.CharField()
-    core_name = models.CharField()
-    middle_name = models.CharField()
-    right_name = models.CharField()
+    left_name = models.CharField(max_length=50)
+    core_name = models.CharField(max_length=3)
+    middle_name = models.CharField(max_length=50)
+    right_name = models.CharField(max_length=50)
     n = models.IntegerField()
     m = models.IntegerField()
     x = models.IntegerField()
@@ -19,10 +19,11 @@ class StandardName(Name):
     def full_name(self):
         nm = 'n%d_m%d' % (self.n, self.m)
         xyz = 'x%d_y%d_z%d' % (self.x, self.y, self.z)
-        return '_'.join([self.left_name, self.core_name, self.middle_name, self.right_side, nm, xyz])
+        return '_'.join([self.left_name, self.core_name, self.middle_name, self.right_name, nm, xyz])
 
 
 class DataPoint(models.Model):
+    name = models.ForeignKey(Name, related_name="data")
     occupied = models.FloatField()
     virtual = models.FloatField()
     homo_orbital = models.IntegerField()
