28fa30fc898b6f529e0e3acc92161dac5f849332
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-03-10 23:27:07 -0400

    Move all of run.py into the __main__ block

--- run.py
+++ run.py
@@ -10,52 +10,52 @@ import ml.ml
 from ml.display import plot_scan_2d, plot_scan, plot_num_samples
 from ml.display import PCA_stuff, plot_PCA_background
 
-data = []
-with open("cleaned_data.csv", "r") as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for row in reader:
-        temp = [row[3]]
-        for item in row[4:]:
-            try:
-                x = ast.literal_eval(item)
-                if x == []:
-                    break
-                temp.append(x)
-            except:
-                pass
-
-        if len(temp) == 9:
-            data.append(temp)
-
-M = len(data)
-
-HOMO = numpy.zeros((M, 1))
-LUMO = numpy.zeros((M, 1))
-DIPOLE = numpy.zeros((M, 1))
-ENERGY = numpy.zeros((M, 1))
-GAP = numpy.zeros((M, 1))
-TIME = numpy.zeros((M, 1))
-
-features = []
-for i, (name, feat, occ, virt, orb, dip, eng, gap, time) in enumerate(data):
-    features.append(feat)
-    HOMO[i] = occ
-    LUMO[i] = virt
-    DIPOLE[i] = dip
-    ENERGY[i] = eng
-    GAP[i] = gap
-    TIME[i] = time
-YSETS = (HOMO, LUMO, GAP)
-
-FEATURES = []
-for group in zip(*tuple(features)):
-    FEATURES.append(numpy.matrix(group))
-FEATURES1 = ml.ml.get_extended_features(HOMO, LUMO, GAP, FEATURES[1:])
-
-
-results, results2, clfs, clfs2 = ml.ml.main(YSETS, FEATURES, FEATURES1)
 
 if __name__ == "__main__":
+    data = []
+    with open("cleaned_data.csv", "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            temp = [row[3]]
+            for item in row[4:]:
+                try:
+                    x = ast.literal_eval(item)
+                    if x == []:
+                        break
+                    temp.append(x)
+                except:
+                    pass
+
+            if len(temp) == 9:
+                data.append(temp)
+
+    M = len(data)
+
+    HOMO = numpy.zeros((M, 1))
+    LUMO = numpy.zeros((M, 1))
+    DIPOLE = numpy.zeros((M, 1))
+    ENERGY = numpy.zeros((M, 1))
+    GAP = numpy.zeros((M, 1))
+    TIME = numpy.zeros((M, 1))
+
+    features = []
+    for i, (name, feat, occ, virt, orb, dip, eng, gap, time) in enumerate(data):
+        features.append(feat)
+        HOMO[i] = occ
+        LUMO[i] = virt
+        DIPOLE[i] = dip
+        ENERGY[i] = eng
+        GAP[i] = gap
+        TIME[i] = time
+    YSETS = (HOMO, LUMO, GAP)
+
+    FEATURES = []
+    for group in zip(*tuple(features)):
+        FEATURES.append(numpy.matrix(group))
+    FEATURES1 = ml.ml.get_extended_features(HOMO, LUMO, GAP, FEATURES[1:])
+
+    results, results2, clfs, clfs2 = ml.ml.main(YSETS, FEATURES, FEATURES1)
+
     # Make some example plots
     2d_scan_params = {
         "C": [0.05, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000],
